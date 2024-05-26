8d786d54beb95eae8f099330223567f7f745b255
ssk2024 <saiskotra24@gmail.com>
2024-05-26 15:52:22 -0500

    Added redirect to resume from Header

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -3,11 +3,15 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Header = () => {
+  const openResume = () => {
+    window.open("/resume.pdf", "_blank");
+  };
+
   return (
     <div className="flex justify-between p-4 bg-black text-white">
       <NavLink to="/" className="btn">Kavya Kotra</NavLink>
       <div className="flex">
-        <NavLink to="/resume" className="right-btn">Resume</NavLink>
+        <button className="right-btn resume-btn" onClick={openResume}>Resume</button>
         <NavLink to="/experience" className="right-btn">Experience</NavLink>
         <NavLink to="/aboutme" className="right-btn">About Me</NavLink>
       </div>
--- src/pages/ResumePage.js
+++ src/pages/ResumePage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const ResumePage = () => {
+
   return (
     <div className="flex flex-col h-screen bg-black text-white justify-center items-center">
       <p>Resume Page</p>
